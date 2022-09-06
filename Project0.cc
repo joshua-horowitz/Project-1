8e67078dc63cb09f1302b646d046c3e39637b523
joshua-horowitz <112425813+joshua-horowitz@users.noreply.github.com>
2022-09-06 13:49:16 -0400

    Add files via upload

---
+++ Project0.cc
@@ -0,0 +1,8 @@
+char * str = "char * str = %c%s%c;%c%c#include <iostream>%c%cint main() {%cprintf(str,34,str,34,10,10,10,10,10,10,10,10);%creturn 0;%c}%c";
+
+#include <iostream>
+
+int main() {
+printf(str,34,str,34,10,10,10,10,10,10,10,10);
+return 0;
+}
