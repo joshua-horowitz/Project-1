97d8fe386a65dadfd9eeb23c86717df4c0da4ae6
Joshua Horowitz <jhorowit@hamilton.edu>
2022-09-07 20:57:57 -0400

    This is a commit for my MAIN branch.

--- Project0.cc
+++ Project0.cc
@@ -1,7 +1,7 @@
 char * str = "char * str = %c%s%c;%c%c#include <iostream>%c%cint main() {%cprintf(str,34,str,34,10,10,10,10,10,10,10,10);%creturn 0;%c}%c";
 
 #include <iostream>
- 
+// This is a comment for my MAIN branch. 
 int main() {
 printf(str,34,str,34,10,10,10,10,10,10,10,10);
 return 0;
